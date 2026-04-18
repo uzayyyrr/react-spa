850d3102bf8a166bf94b4ed6999831f599cf0a29
Uzayr Sheikh <uzayyyrr@gmail.com>
2026-04-18 22:23:59 +0530

    final fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import { HashRouter, Routes, Route, Link, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Products from "./pages/Products";
 import Login from "./pages/Login";
+import Products from "./pages/Products";
 
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+function App() {
   return (
     <HashRouter>
-      <h1>Grocery SPA</h1>
-      <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link> | 
-        <Link to="/products">Products</Link> | 
-        {!isLoggedIn ? <Link to="/login">Login</Link> :
-        <button onClick={()=>setIsLoggedIn(false)}>Logout</button>}
-      </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/products" element={isLoggedIn ? <Products /> : <Navigate to="/login"/>} />
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </HashRouter>
   );
 }
+
+export default App;
