d9e0b66e355689c8f1a23581418da1b425248e9e
Uzayr Sheikh <uzayyyrr@gmail.com>
2026-04-18 22:32:35 +0530

    added redirects for clean URL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
@@ -6,14 +6,14 @@ import Products from "./pages/Products";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/products" element={<Products />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
